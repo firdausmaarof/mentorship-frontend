91195bcf29acf3e29c0a39f46371fd1620fdb536
Mohamad Firdaus Maarof <firdausmaarof93@gmail.com>
2020-10-22 12:52:12 +0800

    add rsuite (#1)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 const withPWA = require('next-pwa');
+const withLess = require('@zeit/next-less');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-  },
-});
+module.exports = withPWA(
+  withLess({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+    },
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      dest: 'public',
+    },
+  })
+);
